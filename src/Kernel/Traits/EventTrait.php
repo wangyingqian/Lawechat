0db8c2e104b6203849926843b40b6f0d745f41d8
汪应乾 <891699279@qq.com>
2019-01-25 18:01:10 +0800

    优化

--- src/Kernel/Traits/EventTrait.php
+++ src/Kernel/Traits/EventTrait.php
@@ -1,7 +1,55 @@
 <?php
 namespace Lawechat\Kerner\Traits;
 
+use Illuminate\Events\Dispatcher;
+
 trait EventTrait
 {
+    private $dispatcher;
+
+    protected $listenerClass;
+
+    protected $listeners = [];
+
+    /**
+     * 注册事件监听
+     *
+     * @return bool
+     */
+    protected function registerEvent()
+    {
+        if (empty($this->listenerClass) || empty($this->dispatcher)){
+            return true;
+        }
+
+        if (!class_exists($this->listenerClass)){
+            throw new \RuntimeException('事件监听器定义错误');
+        }
+
+        if ($this->dispatcher instanceof Dispatcher){
+            foreach ($this->listeners as $listener => $event){
+                $this->dispatcher->listen($event, $this->listenerClass.'@'.$listener);
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 触发事件
+     *
+     * @param $listener
+     * @param mixed ...$args
+     *
+     * @return bool
+     */
+    protected function dispatchEvent($listener, ...$args)
+    {
+        if (isset($this->listeners[$listener])){
+            return $this->dispatcher->dispatch(new $this->listeners[$listener](...$args));
+        }
+
+        return true;
+    }
 
 }
---
+++ src/Kernel/Traits/JobTrait.php
@@ -0,0 +1,18 @@
+<?php
+namespace Lawechat\Kerner\Traits;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+trait JobTrait
+{
+    private $job;
+
+    protected function dispatchJob($className, $data, $queue = 'default')
+    {
+        if (class_implements($className, ShouldQueue::class) && method_exists($className, 'dispatch')){
+            $this->job = $className::dispatch($data)->onQueue($queue);
+        }
+
+        return $this;
+    }
+}
